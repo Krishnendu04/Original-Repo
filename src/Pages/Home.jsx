4a340ffc1a881aa84d31f134b7cc523d2f9edf30
krishnendu04 <krishnendupurkait0@gmail.com>
2026-01-31 11:49:18 +0530

    Changed handling in feature branch

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>This is home page added</h1>
+      <h1>This is home page added for newly feature branch</h1>
     </div>
   )
 }
