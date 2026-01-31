b855abbfef4307d185826a1960236aabf59cc38b
Krishnendu04 <111035391+Krishnendu04@users.noreply.github.com>
2026-01-31 11:12:22 +0530

    Update Counter.jsx added decrease count button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react'
 
 const Counter = () => {
   const [count,setCount]=useState(0);
-  const handleCount=()=>{
+  const handleIncrease=()=>{
     setCount(prev=>prev+1);
   }
+  const handleDecrease=()=>{
+    setCount(prev=>prev-1);
   return (
     <div>
       <p>Counter:{count}</p>
-      <button onClick={handleCount}>Count</button>
+      <button onClick={handleIncrease}>IncreaseCount</button>
+      <button onClick={handleDecrease}>DecreaseCount</button>
     </div>
   )
 }
