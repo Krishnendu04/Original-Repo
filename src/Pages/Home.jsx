5172f21d48f9638835e84d0699f2b575e4874077
krishnendu04 <krishnendupurkait0@gmail.com>
2026-01-31 12:06:12 +0530

    added coflict

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,6 @@ const Home = () => {
   return (
     <div>
       <h1>This is home page added for newly feature branch</h1>
-      <h1>This is home page added for new feature in main</h1>
     </div>
   )
 }
